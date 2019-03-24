1984f09e268a350568380c52ca7643e497febc0a
rgu1602399 <1602399@rgu.ac.uk>
2019-03-24 15:30:40 +0000

    indent local search library

--- search/cm3038/search/local/State.java
+++ search/cm3038/search/local/State.java
@@ -8,19 +8,18 @@ import java.util.*;
  *
  * @author Kit-ying Hui
  */
-public interface State
-{
-/**
- * Converts the state into a {@link String}. This method controls how a state will be printed.
- * @return A {@link String} representation of the state.
- */
-public String toString();
+public interface State {
+    /**
+     * Converts the state into a {@link String}. This method controls how a state will be printed.
+     * @return A {@link String} representation of the state.
+     */
+    public String toString();
 
-/**
- * Return all neighbouring states of the current one.
- * These neighbours are obtained by changing each variable in the current state.
- * 
- * @return All neighbouring states as a Set.
- */
-public Set<State> neighbours();
-} //end interface
+    /**
+     * Return all neighbouring states of the current one.
+     * These neighbours are obtained by changing each variable in the current state.
+     * 
+     * @return All neighbouring states as a Set.
+     */
+    public Set<State> neighbours();
+}
